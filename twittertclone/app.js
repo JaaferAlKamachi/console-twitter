ccca0d822cc8459f1b528972901c0d6b0bb10e50
JaaferAlKamachi <44309983+JaaferAlKamachi@users.noreply.github.com>
2019-01-17 17:39:17 +0300

    Update app.js

--- twittertclone/app.js
+++ twittertclone/app.js
@@ -1,10 +1,8 @@
 let program = require('commander');
 let UserConfig = require('./sources/userconfig')
 var { prompt } = require('inquirer');
-let { logininfo, registerinfo, tweetinfo, commentinfo, tweetListinfo, tweetControlinfo } = require('./sources/info');
+let { logininfo, registerinfo } = require('./sources/info');
 let UserController = require('./sources/controllers/usercontrol');
-let TweetController = require('./sources/controllers/tweetcontrol');
-let CommentController = require('./sources/controllers/commentcontrol');
 
 
 
@@ -57,57 +55,6 @@ program
         })
     });
 
-program
-    .command('tweet')
-    .alias('tw')
-    .description('Create Tweet')
-    .action(() => {
-        UserConfig.checkLogin((uid) => {
-            if (uid) {
-                prompt(tweetinfo).then(answers => {
-                    answers.uid = uid
-                    let tweetController = new TweetController();
-                    tweetController.createTweet(answers)
-                })
-            } else {
-                console.log("you are not login !")
-            }
-        })
-    });
-program
-    .command('home')
-    .alias('h')
-    .description('Show all Tweets')
-    .action(() => {
-        UserConfig.checkLogin((uid) => {
-            if (uid) {
-                let tweetController = new TweetController();
-                tweetController.showTweets((tw) => {
-                    let tweets = []
-                    tw.forEach(function (doc) {
-                        tweets.push(doc.data().content + "\t});
-                    prompt(tweetListinfo(tweets)).then((answers) => {
-                        let id = answers.id.split("\n")[1]
-                        prompt(tweetControlinfo(tweets)).then((answers) => {
-                            let commentController = new CommentController();
-                            if (answers.control == 'New Comment') {
-                                
-                                prompt(commentinfo).then((answers) => {
-                                    answers.tweetId = id
-                                    answers.uid = uid
-                                    commentController.createComment(answers)
-                                })
-                            }
-                            if(answers.control == 'Show Comments'){commentController.showComments(id);}
-                        })
-                    })
-                })
-
-            } else {
-                console.log("you are not login !")
-            }
-        })
-    });
 
 
 
